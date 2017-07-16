130447a09f0d4560858ca68d56750f07b0a7f9f0
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-07-16 18:53:44 +0300

    Updated models - User and Car

--- app/Entity/Car.php
+++ app/Entity/Car.php
@@ -1,9 +1,42 @@
 <?php
-
 namespace App\Entity;
 
+use Illuminate\Database\Eloquent\Model;
 
-class Car
+/**
+ * Class Car
+ * @package App\Entity
+ */
+class Car extends Model
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'cars';
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'model',
+        'registration_number',
+        'year',
+        'color',
+        'mileage',
+        'price',
+    ];
+
+    /**
+     * Get the user that owns the car.
+     *
+     * @return [type] [description]
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Entity/User.php
+++ app/Entity/User.php
@@ -1,9 +1,35 @@
 <?php
-
 namespace App\Entity;
 
+use Illuminate\Database\Eloquent\Model;
 
-class User
+/**
+ * Class User
+ * @package App\Entity
+ */
+class User extends Model
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['first_name', 'last_name', 'is_active'];
+
+    /**
+     * Get the cars for the user.
+     *
+     * @return [type] [description]
+     */
+    public function cars()
+    {
+        return $this->hasMany(Car::class);
+    }
 }
