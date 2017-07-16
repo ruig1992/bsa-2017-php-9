7ddf2550855ac887ed735d2e5aca40334a23f4c3
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-07-16 18:50:34 +0300

    Added DB migrations

---
+++ database/migrations/2017_07_16_154254_create_cars_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('model');
+            $table->string('registration_number');
+            $table->string('color');
+            $table->integer('year')->unsigned()->nullable();
+            $table->integer('mileage')->unsigned()->nullable();
+            $table->decimal('price', 10, 2)->nullable();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
