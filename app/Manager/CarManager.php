1cd16717e479026a0944d6d5b7c35780ef96c1e4
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-07-16 19:02:33 +0300

    Added methods to CarManager

--- app/Manager/CarManager.php
+++ app/Manager/CarManager.php
@@ -1,11 +1,68 @@
 <?php
-
 namespace App\Manager;
 
-
+use App\Entity\Car;
+use Illuminate\Support\Collection;
+use App\Request\Contract\SaveCarRequest;
 use App\Manager\Contract\CarManager as CarManagerContract;
 
+/**
+ * Class CarManager
+ * @package App\Manager
+ */
 class CarManager implements CarManagerContract
 {
+    /**
+     * @inheritdoc
+     */
+    public function findAll(): Collection
+    {
+        return Car::all();
+    }
 
+    /**
+     * @inheritdoc
+     */
+    public function findById(int $id): ?Car
+    {
+        return Car::find($id);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function findCarsFromActiveUsers(): Collection
+    {
+        return Car::whereHas('user', function ($query) {
+            $query->where('is_active', true);
+        })->get();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function saveCar(SaveCarRequest $request): ?Car
+    {
+        $car = $request->getCar();
+
+        $car->model = $request->getModel() ?? $car->model;
+        $car->registration_number = $request->getRegistrationNumber() ??
+            $car->registration_number;
+        $car->color = $request->getColor() ?? $car->color;
+        $car->year = $request->getYear() ?? $car->year;
+        $car->mileage = $request->getMileage() ?? $car->mileage;
+        $car->price = $request->getPrice() ?? $car->price;
+        $car->userId = $request->getUser()->id ?? $car->userId;
+
+        return $car->save() ? $car : null;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function deleteCar(int $carId): void
+    {
+        $car = Car::find($carId);
+        $car->delete();
+    }
 }
